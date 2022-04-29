bda01fa1ab5edf9b582c49c8163f6e576e407366
JustAPPin <justappin@Justins-MacBook-Pro.local>
2022-04-29 11:59:50 -0500

    small additions

---
+++ client/src/Components/navBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import React from "react";
+const navBar = () => {
+  return <h1>Nav Bar</h1>;
+};
+export default navBar;
---
+++ client/src/Pages/auth/SignIn.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const SignIn = () => {
+  return <h1>Name</h1>;
+};
+export default SignIn;
