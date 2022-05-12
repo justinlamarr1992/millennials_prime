3bc812de545503b4bd3527e1f8b22b88f646100e
JustAPPin LLC <justappinllc@gmail.com>
2022-05-12 13:38:47 -0500

    profile page

--- client/src/Components/user/ProfileModal.jsx
+++ client/src/Components/user/ProfileModal.jsx
@@ -3,16 +3,28 @@ import User from "../../Assets/Images/user.jpeg";
 
 const ProfileModal = () => {
   return (
-    <div className="right-modal verified-modal p-con-shade">
-      <div className="modal-t-boxes">
-        <img className="info-pic" src={User} alt="User Image here" />
+    <div className="right-modal  p-con-shade">
+      <div className="modal-container">
+        <div className="modal-t-boxes">
+          <div className="noti-box top-boxes">
+            <h3 className="noti-num">2</h3>
+          </div>
+          <img
+            className="prof-pic top-boxes"
+            src={User}
+            alt="User Image here"
+          />
+        </div>
+        <div className="modal-prof-info">
+          <img className="modal-prof-pic" src={User} alt="User Image here" />
+          <h3 className="prof-info-text">Justin Williams</h3>
+          <h5 className="text-gray prof-info-text">@justinWilliams</h5>
+        </div>
+        <div>Connected Button</div>
+        <div>Info on the profile</div>
+        <div>Connected Userd title</div>
+        <div>Conneected Users</div>
       </div>
-      <div>User Picture</div>
-      <div>User Name</div>
-      <div>Connected Button</div>
-      <div>Info on the profile</div>
-      <div>Connected Userd title</div>
-      <div>Conneected Users</div>
     </div>
   );
 };
