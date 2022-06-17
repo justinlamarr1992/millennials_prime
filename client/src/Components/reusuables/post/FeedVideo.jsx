a64f5edc26138959eb70101f82ec54bc82d0774d
JustAPPin LLC <justappinllc@gmail.com>
2022-06-17 12:17:39 -0500

    Episode Update

--- client/src/Components/reusuables/post/FeedVideo.jsx
+++ client/src/Components/reusuables/post/FeedVideo.jsx
@@ -7,12 +7,14 @@ const FeedVideo = () => {
     <section className="post-video-container norm-container con-shade ">
       <ItemUserInfo className="pr-user-info item-user-info" />
       {/* <h3 className="item-user-content">{title}</h3> */}
-      {/* <h3 className="item-user-content">This is the Title of the Video Post</h3> */}
+      <h3 className="feed-post-video-title">
+        This is the Title of the Video Post
+      </h3>
 
       {/* <h5 className="item-user-status text-gray">{status}</h5> */}
-      <h5 className="feed-post-text text-gray">
+      {/* <h5 className="feed-post-text text-gray">
         This is the Status of the Video post
-      </h5>
+      </h5> */}
       <Video />
       <PostLikeDisLike />
     </section>
--- client/src/Pages/User/User.jsx
+++ client/src/Pages/User/User.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import FeedEpisode from "../../Components/reusuables/post/FeedEpisode";
 import FeedPhoto from "../../Components/reusuables/post/FeedPhoto";
 import FeedText from "../../Components/reusuables/post/FeedText";
 import FeedVideo from "../../Components/reusuables/post/FeedVideo";
 import PostLikeDisLike from "../../Components/reusuables/post/PostLikeDislike";
+import PrimeUpdateVideo from "../../Components/reusuables/post/PrimeUpdateVideo";
 import SearchBar from "../../Components/reusuables/SearchBar";
 import ProfileModal from "../../Components/user/ProfileModal";
 
 import "../../Components/user/user.css";
-import Video from "../../Components/video/Video";
 
 const User = () => {
   const [modal, setModal] = useState(true);
@@ -21,36 +22,13 @@ const User = () => {
         {/* Find a way to make this into component but have infor pasted in */}
         <div className="prime-video">
           <SearchBar />
-          <section
-            id="prime"
-            className="prime-container news-container p-con-shade "
-          >
-            <h2 className="pr-title title-space">Weekly Progress Video</h2>
-            <Video />
-            {/* <UserPostInfo className="pr-user-info" /> */}
-            <div className="pr-video-info">
-              <h3>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Numquam, incidunt.
-              </h3>
-              <h5 className="text-gray">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Sapiente expedita odio provident harum accusamus unde quis
-                facere blanditiis, delectus aliquam commodi at quia eius, a
-                iure. Accusamus laborum temporibus et dolor voluptatum tempore
-                suscipit sed quibusdam ipsa, ducimus hic quis.
-              </h5>
-            </div>
-            <div className="pr-like-dislike">
-              <PostLikeDisLike />
-            </div>
-          </section>
+          <PrimeUpdateVideo />
+
           <h1>Toggle Feed Selections</h1>
           <FeedText />
           <FeedPhoto />
           <FeedVideo />
-          <h1>Post Photos</h1>
-          <h1>Post Shows</h1>
+          <FeedEpisode />
           <h1>Music</h1>
           <h1>Store</h1>
         </div>
