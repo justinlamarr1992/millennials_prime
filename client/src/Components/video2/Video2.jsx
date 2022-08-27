f14d8a83b8981d1fb66b579ac98504dd422278bb
Justin <justinlamarr1992@icloud.com>
2022-08-26 20:05:40 -0500

    youtube video palyer volume

--- client/src/Components/video2/Video2.jsx
+++ client/src/Components/video2/Video2.jsx
@@ -20,23 +20,53 @@ const Video2 = () => {
     vidContainerRef.current.classList.remove("paused");
   };
 
+  // TODO: Work out kinks with even listener in react
+  // Key Down play pause
+  // document.addEventListener("keydown", (e) => {
+  //   switch (e.key.toLowerCase()) {
+  //     case " ":
+  //     case "k":
+  //       togglePlay();
+  //       break;
+  //     case "f":
+  //       toggleFullScreen();
+  //       break;
+  //     case "t":
+  //       toggleTheater();
+  //       break;
+  //     case "i":
+  //       toggleMiniPlayer();
+  //       break;
+  //   }
+  // });
   const keyDown = (e) => {
+    const tagName = document.activeElement.tagName.toLowerCase();
+    if (tagName === "input") return;
     switch (e.key.toLowerCase()) {
+      case " ":
+        if (tagName === "button") return;
       case "k":
         togglePlay();
+        break;
+      case "f":
+        toggleFullScreen();
+        break;
+      case "t":
+        toggleTheater();
+        break;
+      case "i":
+        toggleMiniPlayer();
         break;
     }
   };
 
-  // Theater
+  // Theater Mode
   const toggleTheater = () => {
     vidContainerRef.current.classList.toggle("theater");
   };
   const toggleFullScreen = () => {
     const elem = vidContainerRef.current;
-    // setFullScreen(!fullScreen);
     if (!document.mozFullScreen && !document.webkitIsFullScreen) {
-      console.log("Full Screen");
       vidContainerRef.current.classList.add("full-screen");
       if (elem) {
         //checking if ref was initiated
@@ -51,35 +81,42 @@ const Video2 = () => {
         }
       }
     } else {
-      console.log("small Screen");
       vidContainerRef.current.classList.remove("full-screen");
-      if (elem) {
-        if (elem.exitFullscreen) {
-          elem.exitFullscreen();
-        } else if (elem.msExitFullscreen) {
-          elem.msExitFullscreen();
-        } else if (elem.mozCancelFullScreen) {
-          elem.mozCancelFullScreen();
-        } else if (elem.webkitExitFullscreen) {
-          elem.webkitExitFullscreen();
-        }
-      }
+      document.exitFullscreen();
+      console.log("exiting full screen");
     }
-
-    // if (elem) {
-    //   //checking if ref was initiated
-    //   if (elem.requestFullscreen) {
-    //     elem.requestFullscreen();
-    //   } else if (elem.webkitRequestFullscreen) {
-    //     /* Safari */
-    //     elem.webkitRequestFullscreen();
-    //   } else if (elem.msRequestFullscreen) {
-    //     /* IE11 */
-    //     elem.msRequestFullscreen();
-    //   }
-    // }
   };
-  const toggleMiniPlayer = () => {};
+  const toggleMiniPlayer = () => {
+    // const elem = vidContainerRef.current;
+    const vid = vidRef.current;
+    // if (!document.mozFullScreen && !document.webkitIsFullScreen) {
+    if (vidContainerRef.current.classList.contains("mini-player")) {
+      document.exitPictureInPicture();
+      console.log("exited");
+      vidContainerRef.current.classList.remove("mini-player");
+    } else {
+      vid.requestPictureInPicture();
+      console.log("requested");
+      vidContainerRef.current.classList.add("mini-player");
+    }
+    //   vidContainerRef.current.classList.add("full-screen");
+    //   if (elem) {
+    //     //checking if ref was initiated
+    //     if (elem.requestFullscreen) {
+    //       elem.requestFullscreen();
+    //     } else if (elem.webkitRequestFullscreen) {
+    //       /* Safari */
+    //       elem.webkitRequestFullscreen();
+    //     } else if (elem.msRequestFullscreen) {
+    //       /* IE11 */
+    //       elem.msRequestFullscreen();
+    //     }
+    //   }
+    // } else {
+    //   vidContainerRef.current.classList.remove("full-screen");
+    //   document.exitFullscreen();
+    //   console.log("exiting full screen");}
+  };
   return (
     <div
       className="video-container paused"
@@ -95,6 +132,26 @@ const Video2 = () => {
             </svg>
             <svg class="pause-icon" viewBox="0 0 24 24">
               <path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
+            </svg>
+          </button>
+          <button className="mute-btn">
+            <svg class="volume-high-icon" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"
+              />
+            </svg>
+            <svg class="volume-low-icon" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M5,9V15H9L14,20V4L9,9M18.5,12C18.5,10.23 17.5,8.71 16,7.97V16C17.5,15.29 18.5,13.76 18.5,12Z"
+              />
+            </svg>
+            <svg class="volume-muted-icon" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M12,4L9.91,6.09L12,8.18M4.27,3L3,4.27L7.73,9H3V15H7L12,20V13.27L16.25,17.53C15.58,18.04 14.83,18.46 14,18.7V20.77C15.38,20.45 16.63,19.82 17.68,18.96L19.73,21L21,19.73L12,10.73M19,12C19,12.94 18.8,13.82 18.46,14.64L19.97,16.15C20.62,14.91 21,13.5 21,12C21,7.72 18,4.14 14,3.23V5.29C16.89,6.15 19,8.83 19,12M16.5,12C16.5,10.23 15.5,8.71 14,7.97V10.18L16.45,12.63C16.5,12.43 16.5,12.21 16.5,12Z"
+              />
             </svg>
           </button>
           <button className="mini-player-btn" onClick={toggleMiniPlayer}>
@@ -141,6 +198,7 @@ const Video2 = () => {
         onPlay={videoPlay}
         onPause={videoPause}
         onClick={togglePlay}
+        on
       ></video>
     </div>
   );
