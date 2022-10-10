57ee360a5caa5907743e155d0441a32d0bdf2c62
JustAPPin LLC <justappinllc@gmail.com>
2022-10-09 22:54:50 -0500

    Dave Gray node tut finished

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,16 +1,8 @@
-const usersDb = {
-  users: require("../models/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-
+const UserTest = require("../models/User");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const fsPromises = require("fs").promises;
-const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { user, password } = req.body;
@@ -21,78 +13,17 @@ const handleLogin = async (req, res) => {
       .status(400)
       .json({ message: "Both Email and Password required" });
 
-  const foundUser = await usersDb.users.find(
-    (person) => person.username === user
-  );
+  const foundUser = await UserTest.findOne({ username: user }).exec();
   if (!foundUser) return res.sendStatus(401); //Unauthrized
   //   evealuate password
   const match = await bcrypt.compare(password, foundUser.password);
-  // if (match) {
-  //   const roles = Object.values(foundUser.roles).filter(Boolean);
-  //   console.log("It ran here");
-  //   // create JWT
-  //   const accessToken = jwt.sign(
-  //     {
-  //       UserInfo: {
-  //         username: foundUser.username,
-  //         roles: roles,
-  //       },
-  //     },
-  //     process.env.SECRET,
-  //     // TODO: SUPER SHORT CHANGE THIS
-  //     { expiresIn: "24h" }
-  //   );
-  //   const newRefreshToken = jwt.sign(
-  //     { username: foundUser.username },
-  //     process.env.SECRET,
-  //     // TODO: SUPER SHORT CHANGE THIS
-  //     { expiresIn: "1d" }
-  //   );
-  //   let newRefreshTokenArray = !cookies?.jwt
-  //     ? foundUser.refreshToken
-  //     : foundUser.refreshToken.filter((rt) => rt !== cookies.jwt);
-  //   if (cookies?.jwt) {
-  //     /*
-  //           Scenario added here:
-  //               1) User logs in but never uses RT and does not logout
-  //               2) RT is stolen
-  //               3) If 1 & 2, reuse detection is needed to clear all RTs when user logs in
-  //           */
-  //     const refreshToken = cookies.jwt;
-  //     const foundToken = await User.findOne({ refreshToken }).exec();
-  //     // detected refreshtoken reuse
-  //     if (!foundToken) {
-  //       // clearout all other tokens
-  //       newRefreshTokenArray = [];
-  //     }
-  //     res.clearCookie("jwt", {
-  //       httpOnly: true,
-  //       sameSite: "None",
-  //       secure: true,
-  //     });
-  //   }
-  //   // saving refresh token for current user
-  //   foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
-  //   const result = await foundUser.save();
-  //   // create Secure Cookie with current user
-  //   res.cookie("jwt", newRefreshToken, {
-  //     httpOnly: true,
-  //     secure: true,
-  //     sameSite: "None",
-  //     maxAge: 24 * 60 * 60 * 1000,
-  //   });
-  //   // send authorization roles and access token to user
-  //   res.json({ accessToken, roles });
-  // } else {
-  //   res.sendStatus(401);
-  // }
   if (match) {
     const roles = Object.values(foundUser.roles);
     //create JWTs
     const accessToken = jwt.sign(
       { UserInfo: { username: foundUser.username, roles: roles } },
       process.env.SECRET,
-      { expiresIn: "30s" }
+      { expiresIn: "180s" }
     );
     const refreshToken = jwt.sign(
       { username: foundUser.username },
@@ -101,226 +32,23 @@ const handleLogin = async (req, res) => {
     );
 
     // save refreshtoken with current user
-    const otherUsers = usersDb.users.filter(
-      (person) => person.username !== foundUser.username
-    );
-    const currentUser = { ...foundUser, refreshToken };
-    usersDb.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "models", "users.json"),
-      JSON.stringify(usersDb.users)
-    );
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+    console.log(result);
+
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
       sameSite: "None",
+      // secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
+    // TODO: uncomment secure for production
     res.json({ accessToken });
   } else {
     res.sendStatus(401);
   }
-  // if (!cookies) {
-  //   console.log("We have No End Cookies");
-  // } else {
-  //   console.log("The End cookies are", cookies);
-  // }
-};
-const login = async (req, res) => {
-  //   const cookies = req.cookies;
-  //   if (!cookies) {
-  //     console.log("We have No Beginning Cookies");
-  //   } else {
-  //     console.log("The Beginning cookies are", cookies);
-  //   }
-  //   const { user, password } = req.body;
-  //   console.log(req.body);
-  //   // console.log(user, password);
-  //   if (!user || !password)
-  //     return res
-  //       .status(400)
-  //       .json({ message: "Both Email and Password required" });
-  //   const foundUser = await User.findOne({ username: user }).exec();
-  //   if (!foundUser) return res.sendStatus(401); //Unauthrized
-  //   //   evealuate password
-  //   const match = await bcrypt.compare(password, foundUser.password);
-  //   if (match) {
-  //     const roles = Object.values(foundUser.roles).filter(Boolean);
-  //     console.log("It ran here");
-  //     // create JWT
-  //     const accessToken = jwt.sign(
-  //       {
-  //         UserInfo: {
-  //           username: foundUser.username,
-  //           roles: roles,
-  //         },
-  //       },
-  //       process.env.SECRET,
-  //       // TODO: SUPER SHORT CHANGE THIS
-  //       { expiresIn: "24h" }
-  //     );
-  //     const newRefreshToken = jwt.sign(
-  //       { username: foundUser.username },
-  //       process.env.SECRET,
-  //       // TODO: SUPER SHORT CHANGE THIS
-  //       { expiresIn: "1d" }
-  //     );
-  //     let newRefreshTokenArray = !cookies?.jwt
-  //       ? foundUser.refreshToken
-  //       : foundUser.refreshToken.filter((rt) => rt !== cookies.jwt);
-  //     if (cookies?.jwt) {
-  //       /*
-  //             Scenario added here:
-  //                 1) User logs in but never uses RT and does not logout
-  //                 2) RT is stolen
-  //                 3) If 1 & 2, reuse detection is needed to clear all RTs when user logs in
-  //             */
-  //       const refreshToken = cookies.jwt;
-  //       const foundToken = await User.findOne({ refreshToken }).exec();
-  //       // detected refreshtoken reuse
-  //       if (!foundToken) {
-  //         // clearout all other tokens
-  //         newRefreshTokenArray = [];
-  //       }
-  //       res.clearCookie("jwt", {
-  //         httpOnly: true,
-  //         sameSite: "None",
-  //         secure: true,
-  //       });
-  //     }
-  //     // saving refresh token for current user
-  //     foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
-  //     const result = await foundUser.save();
-  //     // create Secure Cookie with current user
-  //     res.cookie("jwt", newRefreshToken, {
-  //       httpOnly: true,
-  //       secure: true,
-  //       sameSite: "None",
-  //       maxAge: 24 * 60 * 60 * 1000,
-  //     });
-  //     // send authorization roles and access token to user
-  //     res.json({ accessToken, roles });
-  //   } else {
-  //     res.sendStatus(401);
-  //   }
-  //   if (!cookies) {
-  //     console.log("We have No End Cookies");
-  //   } else {
-  //     console.log("The End cookies are", cookies);
-  //   }
-};
-
-const register = async (req, res) => {
-  // const { user, password } = req.body;
-  // if (!user || !password)
-  //   return res.status(400).json({ message: "Email and Password required" });
-  // // check for duplicates usernames in the db
-  // const duplicate = await User.findOne({ username: user }).exec();
-  // if (duplicate) return res.sendStatus(409); //conflict
-  // try {
-  //   //encrypt the password
-  //   const hashedPassword = await bcrypt.hash(password, 10);
-  //   //create and store the new user
-  //   const result = await User.create({
-  //     username: user,
-  //     password: hashedPassword,
-  //   });
-  //   console.log(result);
-  //   res.status(201).json({ success: `New user ${user} created` });
-  // } catch (err) {
-  //   res.status(500).json({ message: err.message });
-  // }
-};
-
-const logout = async (req, res) => {
-  // // on Client also delete accessToken
-  // const cookies = req.cookies;
-  // if (!cookies?.jwt) return res.sendStatus(204); //No content
-  // const refreshToken = cookies.jwt;
-  // // Is refreshtoken in db?
-  // const foundUser = await User.findOne({ refreshToken }).exec();
-  // if (!foundUser) {
-  //   res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
-  //   return res.sendStatus(204);
-  // }
-  // //   Delete refreshToken in db
-  // foundUser.refreshToken = foundUser.refreshToken.filter(
-  //   (rt) => rt !== refreshToken
-  // );
-  // const result = await foundUser.save();
-  // console.log(result);
-  // res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
-  // res.sendStatus(204);
-};
-
-const refreshToken = async (req, res) => {
-  // const cookies = req.cookies;
-  // console.log("The cookies are", cookies);
-  // if (!cookies?.jwt) return res.sendStatus(401);
-  // const refreshToken = cookies.jwt;
-  // res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
-  // const foundUser = await User.findOne({ refreshToken }).exec();
-  // // Detected refresh token reuse!
-  // if (!foundUser) {
-  //   jwt.verify(refreshToken, process.env.SECRET, async (err, decoded) => {
-  //     if (err) return res.sendStatus(403); //Forbidden
-  //     //Delete refresh tokens of hacked user
-  //     const hackedUser = await User.findOne({
-  //       username: decoded.username,
-  //     }).exec();
-  //     hackedUser.refreshToken = [];
-  //     const result = await hackedUser.save();
-  //   });
-  //   return res.sendStatus(403); //Forbidden
-  // }
-  // const newRefreshTokenArray = foundUser.refreshToken.filter(
-  //   (rt) => rt !== refreshToken
-  // );
-  // //evaluate jwt
-  // jwt.verify(refreshToken, process.env.REFRESH_SECRET, async (err, decoded) => {
-  //   if (err) {
-  //     //expired refresh token
-  //     foundUser.refreshToken = [...newRefreshTokenArray];
-  //     const result = await foundUser.save();
-  //   }
-  //   if (err || foundUser.username !== decoded.username)
-  //     return res.sendStatus(403);
-  //   //Refresh token still valid
-  //   const roles = Object.values(foundUser.roles);
-  //   const accessToken = jwt.sign(
-  //     {
-  //       UserInfo: {
-  //         username: decoded.username,
-  //         roles: roles,
-  //       },
-  //     },
-  //     process.env.SECRET,
-  //     // TODO: Super short
-  //     { expiresIn: "10s" }
-  //   );
-  //   const newRefreshToken = jwt.sign(
-  //     { username: foundUser.username },
-  //     process.env.REFRESH_SECRET,
-  //     // TODO: Super short
-  //     { expiresIn: "15s" }
-  //   );
-  //   //Saving refreshToken with current user
-  //   foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
-  //   const result = await foundUser.save();
-  //   //Creates Secure Cookie with refresh token
-  //   res.cookie("jwt", newRefreshToken, {
-  //     httpOnly: true,
-  //     secure: true,
-  //     sameSite: "None",
-  //     maxAge: 24 * 60 * 60 * 1000,
-  //   });
-  //   res.json({ accessToken });
-  // });
 };
 
 module.exports = {
   handleLogin,
-  login,
-  register,
-  logout,
-  refreshToken,
 };
--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,12 +1,13 @@
-const usersDb = {
-  users: require("../models/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
-const fsPromises = require("fs").promises;
-const path = require("path");
+const UserTest = require("../models/User");
 const bcrypt = require("bcrypt");
+// const usersDb = {
+//   users: require("../models/users.json"),
+//   setUsers: function (data) {
+//     this.users = data;
+//   },
+// };
+// const path = require("path");
+// const fsPromises = require("fs").promises;
 
 const handleNewUser = async (req, res) => {
   const { user, password } = req.body;
@@ -14,10 +15,11 @@ const handleNewUser = async (req, res) => {
     return res.status(400).json({ message: "Email and Password required" });
 
   // check for duplicates usernames in the db
-  // const duplicate = await User.findOne({ username: user }).exec();
-  const duplicate = await usersDb.users.find(
-    (person) => person.username === user
-  );
+  const duplicate = await UserTest.findOne({ username: user }).exec();
+
+  // const duplicate = await usersDb.users.find(
+  //   (person) => person.username === user
+  // );
   if (duplicate) return res.sendStatus(409); //conflict
 
   try {
@@ -25,25 +27,24 @@ const handleNewUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // //create and store the new user
-    // const result = await User.create({
-    //   username: user,
-    //   password: hashedPassword,
-    // });
-    // console.log(result);
-    // res.status(201).json({ success: `New user ${user} created` });
+    const result = await UserTest.create({
+      username: user,
+      password: hashedPassword,
+    });
+    console.log(result);
 
     // store the new user
-    const newUser = {
-      username: user,
-      roles: { User: 2001 },
-      password: hashedPassword,
-    };
-    usersDb.setUsers([...usersDb.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "models", "users.json"),
-      JSON.stringify(usersDb.users)
-    );
-    console.log(usersDb.users);
+    // const newUser = {
+    //   username: user,
+    //   roles: { User: 2001 },
+    //   password: hashedPassword,
+    // };
+    // usersDb.setUsers([...usersDb.users, newUser]);
+    // await fsPromises.writeFile(
+    //   path.join(__dirname, "..", "models", "users.json"),
+    //   JSON.stringify(usersDb.users)
+    // );
+    // console.log(usersDb.users);
     res.status(201).json({ success: `New User ${user} created` });
   } catch (err) {
     res.status(500).json({ message: err.message });
