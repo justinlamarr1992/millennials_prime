4f76bfb736f629927b9433ba7a33a699ad74a715
JustAPPin LLC <justappinllc@gmail.com>
2023-06-28 16:02:39 -0500

    Going back need to fix the Modals now

--- client/src/Components/reusuables/modals/MainModal.jsx
+++ client/src/Components/reusuables/modals/MainModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import User from "../../../Assets/Images/user.jpeg";
 import { FaEnvelope } from "react-icons/fa";
 import HomeFollowComp from "./HomeFollowComp";
