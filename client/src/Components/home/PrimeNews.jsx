7d5c9805616d0999999bff5bb38cd738c420c2c2
Justin <justinlamarr1992@icloud.com>
2023-07-01 22:24:25 -0500

    tablet changes

--- client/src/Components/home/PrimeNews.jsx
+++ client/src/Components/home/PrimeNews.jsx
@@ -39,85 +39,6 @@ const PrimeNews = () => {
     getNews();
   }, []);
 
-  // useEffect(() => {
-  //   const getNews = async () => {
-  //     try {
-  //       const response = await axiosPrivate.get(
-  //         `/testUploads/file/${videoFileName}`,
-  //         {}
-  //       );
-  //       if (response.data.success) {
-  //         console.log(response.data);
-  //         setTestDelVideo(response.data.file._id);
-  //         alert("Video Downloaded Successfully");
-  //       } else {
-  //         alert("Failed to Download Video");
-  //       }
-
-  //       // console.log(response.data.video[0]);
-  //       // setActiveVideo({
-  //       //   ...activeVideo,
-  //       //   active: response.data.video[0].filePath,
-  //       // });
-  //       // // setVideoId(response.data.video[0]._id);
-  //       // const newUserId = response.data.video[0].userPosting;
-
-  //       // const getUserInfo = async () => {
-  //       //   try {
-  //       //     const response1 = await axiosPrivate.get(`/users/${newUserId}`);
-
-  //       //     setUserInfo(response1.data);
-  //       //   } catch (err) {
-  //       //     console.log(err);
-  //       //   }
-  //       // };
-
-  //       getUserInfo();
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   getNews();
-
-  //   // return () => {
-  //   //   // this now gets called when the component unmounts
-  //   // };
-  // }, [auth]);
-  // useEffect(() => {
-  //   const getNews = async () => {
-  //     try {
-  //       const response = await axiosPrivate.post("/videos/primenews");
-  //       // console.log(response.data.video[0]);
-  //       console.log(response.data.video[0]);
-  //       setActiveVideo({
-  //         ...activeVideo,
-  //         active: response.data.video[0].filePath,
-  //       });
-  //       // setVideoId(response.data.video[0]._id);
-  //       const newUserId = response.data.video[0].userPosting;
-
-  //       const getUserInfo = async () => {
-  //         try {
-  //           const response1 = await axiosPrivate.get(`/users/${newUserId}`);
-
-  //           setUserInfo(response1.data);
-  //         } catch (err) {
-  //           console.log(err);
-  //         }
-  //       };
-
-  //       getUserInfo();
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   getNews();
-
-  //   // return () => {
-  //   //   // this now gets called when the component unmounts
-  //   // };
-  // }, [auth]);
-  // console.log(video._id);
   const displayName = userInfo.firstName + " " + userInfo.lastName;
 
   console.log(userInfo);
@@ -135,8 +56,8 @@ const PrimeNews = () => {
         className="pr-video p-con-shade"
         // id="d71bda38-3ae5-40aa-8c24-93179412e432"
         src="https://iframe.dacast.com/vod/4cb5f7e0-f945-e8aa-b1a8-62e9bf774b85/d71bda38-3ae5-40aa-8c24-93179412e432"
-        // width="100%"
-        // height="100%"
+        width="100%"
+        height="100%"
         // frameborder="0"
         // scrolling="no"
         // allow="autoplay;encrypted-media"
